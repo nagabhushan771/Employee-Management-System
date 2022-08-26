4e94476af7f042a6f65217fcaf63ab526510ef51
nagabhushan771 <naag000001@gmail.com>
2022-08-26 20:55:41 +0530

    Initial commit of backend

---
+++ employee_management_api/src/test/java/com/practice/projects/Employee/EmployeeManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.projects.Employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
